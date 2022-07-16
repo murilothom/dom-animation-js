d0e351e26be0ac6bff1892895d95013275a5bbd5
murilothom <murilothom.2002@hotmail.com>
2022-07-16 05:12:11 -0300

    secoes aparecendo na tela animacao

--- script.js
+++ script.js
@@ -47,3 +47,20 @@ linksInternos.forEach((link) => {
     // });
   });
 });
+
+// animando ao scrollar da pagina
+
+const sections = document.querySelectorAll(".js-scroll");
+
+function scrollAnim() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const isSectionVisible = sectionTop - window.innerHeight * 0.6 < 0;
+    isSectionVisible && section.classList.add("ativo");
+    !isSectionVisible && section.classList.remove("ativo");
+  });
+}
+
+scrollAnim();
+
+window.addEventListener("scroll", scrollAnim);
