761a1c3822b023cec2a7cbf2e7cedb9fe27c278a
murilothom <murilothom.2002@hotmail.com>
2022-07-18 22:53:48 -0300

    tooltip

---
+++ js/modules/tooltip.js
@@ -0,0 +1,31 @@
+const tooltips = document.querySelectorAll("[data-tooltip]");
+
+const tooltip = () => {
+  tooltips.forEach((item) => {
+    item.addEventListener("mouseover", (e) => {
+      const tooltipBox = document.createElement("div");
+      const text = item.getAttribute("aria-label");
+
+      item.addEventListener("mousemove", cbListenerMove);
+
+      function cbListenerMove(e) {
+        tooltipBox.style.top = e.pageY + 20 + "px";
+        tooltipBox.style.left = e.pageX + 20 + "px";
+      }
+
+      tooltipBox.classList.add("tooltip");
+      tooltipBox.innerText = text;
+      document.body.appendChild(tooltipBox);
+
+      item.addEventListener("mouseleave", cbListenerLeave);
+
+      function cbListenerLeave() {
+        tooltipBox.remove();
+        item.removeEventListener("mouseleave", cbListenerLeave);
+        item.removeEventListener("mousemove", cbListenerMove);
+      }
+    });
+  });
+};
+
+export default tooltip();
--- js/script.js
+++ js/script.js
@@ -3,3 +3,4 @@ import scrollAnim from "./modules/animacao-scroll.js";
 import accordion from "./modules/accordion.js";
 import imgAnim from "./modules/animacao-img.js";
 import modal from "./modules/modal.js";
+import tooltip from "./modules/tooltip.js";
